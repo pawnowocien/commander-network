4da77ef08df5bee0bbc5ee7a04fe31195e870fce
pn459454 <pn459454@students.mimuw.edu.pl>
2026-05-18 13:00:55 +0200

    Improve plotter

--- src/comnet/visualizer/plotter.py
+++ src/comnet/visualizer/plotter.py
@@ -3,16 +3,36 @@ import os
 import matplotlib.pyplot as plt
 import networkx as nx
 
-def save_graph_as_img(g, colors = None, output_file: str = "data/visualized/graph.png"):
+def save_graph_as_img(g, colors = None, output_file: str = "data/visualized/graph.png", show_labels: bool = False):
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
-    plt.figure(figsize=(35, 35))
+    plt.figure(figsize=(30, 30))
     pos = nx.spring_layout(g)
 
-    if colors:
-        _colors = [colors.get(node, "gray") for node in g.nodes()]
-        nx.draw(g, pos, with_labels=True, node_color=_colors, edge_color="gray", node_size=350, linewidths=1, edgecolors="black", font_size=8)
-    else:
-        nx.draw(g, pos, with_labels=False, edge_color="gray", node_size=50, linewidths=1, edgecolors="black")
+    nx.draw_networkx_edges(
+        g,
+        pos,
+        edge_color="black",
+        alpha=0.4
+    )
+
+    node_color = [colors.get(node, "gray") for node in g.nodes()] if colors else "gray"
+    nx.draw_networkx_nodes(
+        g,
+        pos,
+        node_size=100,
+        edgecolors="black",
+        node_color=node_color,
+        linewidths=1
+    )
+
+    if show_labels:
+        nx.draw_networkx_labels(g, pos, font_size=8)
+
+    # if colors:
+    #     _colors = [colors.get(node, "gray") for node in g.nodes()]
+    #     nx.draw(g, pos, with_labels=show_labels, node_color=_colors, edge_color="gray", node_size=350, linewidths=1, edgecolors="black", font_size=8)
+    # else:
+    #     nx.draw(g, pos, with_labels=show_labels, edge_color="gray", node_size=50, linewidths=1, edgecolors="black")
 
     plt.savefig(output_file)
     plt.close()
